5b88ed4c2f15327c8a201fa1173dab2a1fdad71e
Garrison-Pelletier Claude <1142143@decinfo.cvm>
2018-02-06 14:11:50 -0500

    struct

---
+++ js/sprite/Background.js
@@ -0,0 +1,30 @@
+class Background{
+	constructor()
+	{
+		this.x = 0;
+		this.y = 0;
+		this.src ="Spaceball - images/background.png"
+	}
+
+
+	drawBackground(){
+		let bg= new Image();
+		bg.src = this.src;
+
+		if(bg.complete){
+			ctx.drawImage(bg,this.x,this.y)
+		}
+	}
+
+	tick(){
+		this.drawBackground()
+
+	}
+	outSide()
+	{
+		return false;
+	}
+
+
+
+}
