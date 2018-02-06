4eba1dff86162164ccb01d470f0fc77247f8542f
Garrison-Pelletier Claude <1142143@decinfo.cvm>
2018-02-06 14:15:44 -0500

    background

---
+++ js/javascript.js
@@ -0,0 +1,28 @@
+let positionSouris=null;
+let spriteList=[];
+
+
+window.onload=()=>{
+
+	let canvas =document.getElementById("canvas");
+	ctx = canvas.getContext("2d"); //graphics de java!
+
+
+	spriteList.push(new Background());
+	mainTick();
+}
+
+
+function mainTick(){
+	ctx.clearRect(0,0,650,650);
+
+	for (let i = 0; i < spriteList.length; i++) {
+		const element = spriteList[i];
+		element.tick();
+	}
+
+
+	// Rappel (60fps)
+	window.requestAnimationFrame(mainTick);
+
+}
