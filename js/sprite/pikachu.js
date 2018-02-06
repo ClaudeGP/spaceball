ea2edabfc07b9f7748ca432c1350c3dfb0a1acba
Lebrun Karine <1426435@decinfo.cvm>
2018-02-06 15:09:28 -0500

    mouvement

--- js/sprite/pikachu.js
+++ js/sprite/pikachu.js
@@ -10,7 +10,11 @@ class Pikachu {
 			this.sprite = new TiledImage("Spaceball - images/pikachu.png", this.columnCount, this.rowCount, this.refreshDelay, this.loopColumns, this.scale, null);
 			this.sprite.changeRow(2);				// One row per animation
 			this.sprite.changeMinMaxInterval(1, 4);
-			this.speed = 1;
+			this.speedX = 1;
+			this.speedY = -1;
+			this.speed = 1
+			this.x = 50;
+			this.y = 230;
 	}
 			
 	tick() 
@@ -20,26 +24,35 @@ class Pikachu {
 		{
 			this.sprite.changeRow(1);
 			this.sprite.changeMinMaxInterval(1, 4);
-			if (this.speed > 0)
+			if (this.speed >= 0)
 			{
-				this.speed  = -speed
+				this.speed  = -speedX
 			}
 		}
 		if (rightPushed)
 		{
 			this.sprite.changeRow(2);
 			this.sprite.changeMinMaxInterval(1, 4);
-			if (this.speed < 0)
+			if (this.speed <= 0)
 			{
-				this.speed  = -speed
+				this.speed  = speedX
 			}
 		}
 		if (spacePushed)
 		{
 			this.sprite.changeMinMaxInterval(1, 1);
+			//this.y = this.y + this.speedY
+		}
+		if (!leftPushed && !rightPushed && !spacePushed)
+		{
+			this.sprite.changeMinMaxInterval(1, 1);
+			this.speed = 0
 		}
 
-		this.sprite.tick(220, 120, ctx);
+		this.x = this.x + this.speed
+
+
+		this.sprite.tick(this.x, this.y, ctx);
 	}
 }
 			
